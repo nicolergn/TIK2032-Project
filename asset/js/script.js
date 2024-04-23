bfdc5fc847c61bf2d3fabd7d989dbdbec39e0caf
unknown <nicole.pakiding@gmail.com>
2024-04-23 20:12:03 +0800

    Perubahan HTML & CSS, serta menambahkan JS

---
+++ asset/js/script.js
@@ -0,0 +1,14 @@
+// ====================================== NAVBAR - BURGER MENU
+
+let menu = document.querySelector('#burger-menu');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
